db3093201148e58f9b9f41ecb8919cc66ffc0a10
ajkobbi <jeffad1x@gmail.com>
2017-11-29 10:31:20 +0000

    Create mainMenu.cs
    the main menu containing the main features of the game

---
+++ mainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu(){
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e){
+            var playerVPlayer = new PlayerGame.PlayerGame();
+            playerVPlayer.ShowDialog();
+        }
+
+        private void InstructionsButton_Click(object sender, EventArgs e)
+        {
+            var instructions = new Instructions.HowToPlay();
+            instructions.ShowDialog();
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Leaders_button_Click(object sender, EventArgs e)
+        {
+            var Leaders = new Leaders.LeaderBoard();
+            Leaders.ShowDialog();
+        }
+    }
+}
+
